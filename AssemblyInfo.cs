c4b50e897f7ae31aee7f79814af7e94c50f726ba
Travis Illig <travis.illig@silverlining.ventures>
2016-08-01 08:13:47 -0700

    Original code.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("GuidGenConsole")]
+[assembly: AssemblyDescription("Console application for generating GUIDs.")]
+[assembly: AssemblyCompany("Paraesthesia")]
+[assembly: AssemblyCopyright("(c) 2007 Travis Illig")]
+
+[assembly: AssemblyVersion("1.0")]
+
